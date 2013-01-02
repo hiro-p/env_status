0cf639fe9e7421f44bff5785dbf76f67b4728586
Zachary Drummond <zdrummond@biaprotect.com>
2013-01-02 14:37:16 -0800

    Minor cleanup

--- config/initializers/crowd_setup.rb
+++ config/initializers/crowd_setup.rb
@@ -1,5 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :crowd, :crowd_server_url=>ENV['crowd_server_url'], :application_name=>ENV['application_name'], :application_password=>ENV['application_password']
+  provider :crowd, 
+  	:crowd_server_url=>ENV['crowd_server_url'], 
+  	:application_name=>ENV['application_name'], 
+  	:application_password=>ENV['application_password']
 end
 
 OmniAuth.config.on_failure = SessionsController.action(:failure)
